f86fa2ab1f84d570125d2ef0ea40d3cd938d0e2b
Prashanth Reddy <39459337+prashanthr11@users.noreply.github.com>
2022-01-06 10:36:04 +0530

    Time: 128 ms (12.41%), Space: 14.9 MB (18.85%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.py
+++ 1094-car-pooling/1094-car-pooling.py
@@ -1,12 +1,20 @@
 class Solution:
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
+        '''
+        Time Complexity: O(N)
+        Space Complexity: O(1)
+        '''
         trip = [0] * 1001
         
-        for cap, start, end in trips:
-            for i in range(start, end):
-                trip[i] += cap
-                if trip[i] > capacity:
-                    return False
+        for pas, start, end in trips:
+            trip[start] += pas
+            trip[end] -= pas
+            
+        tot = 0
+        for i in trip:
+            tot += i
+            if tot > capacity:
+                return False
             
         return True
     
