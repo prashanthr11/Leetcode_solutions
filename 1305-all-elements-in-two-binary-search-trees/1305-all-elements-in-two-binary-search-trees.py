301075820a66b32e25192b52a02488748ca42010
Prashanth Reddy <39459337+prashanthr11@users.noreply.github.com>
2022-01-26 12:21:50 +0530

    Time: 479 ms (42.32%), Space: 18 MB (81.53%) - LeetHub

--- 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.py
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.py
@@ -7,21 +7,22 @@
 class Solution:
     def getAllElements(self, root1: TreeNode, root2: TreeNode) -> List[int]:
         l = []
-        q = deque([root1, root2])
-        
-        while q:
-            top = q.popleft()
-            
-            if top is None:
-                continue
-                
-            l.append(top.val)
-            
-            if top.left:
-                q.append(top.left)
-                
-            if top.right:
-                q.append(top.right)
-                
-        return sorted(l)
+        def solve(root1, root2):
+            q = deque([root1, root2])
+
+            while q:
+                top = q.popleft()
+
+                if top is None:
+                    continue
+
+                # l.append(top.val)
+                yield top.val
+                if top.left:
+                    q.append(top.left)
+
+                if top.right:
+                    q.append(top.right)
+
+        return sorted(solve(root1, root2))
     
