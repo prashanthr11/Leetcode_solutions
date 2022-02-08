064b0d9a4ac60b8576a526e3957aa919842d9e2a
Prashanth Reddy <39459337+prashanthr11@users.noreply.github.com>
2022-02-08 20:44:28 +0530

    Time: 172 ms (73.01%), Space: 15 MB (29.13%) - LeetHub

--- 1541-minimum-insertions-to-balance-a-parentheses-string/1541-minimum-insertions-to-balance-a-parentheses-string.py
+++ 1541-minimum-insertions-to-balance-a-parentheses-string/1541-minimum-insertions-to-balance-a-parentheses-string.py
@@ -2,27 +2,27 @@ class Solution:
     def minInsertions(self, s: str) -> int:
         '''
         Time Complexity: O(N)
-        Space Complexity: O(N)
+        Space Complexity: O(1)
         '''
-        stk = list()
+        cnt = 0
         ln = len(s)
         i, ret = 0, 0
         
         while i < ln:
             if s[i] == '(':
-                stk.append(s[i])
+                cnt += 1
             else:
                 if i + 1 < ln and s[i + 1] == ')':
                     i += 1
                 else:
                     ret += 1
                     
-                if stk:
-                    stk.pop()
+                if cnt:
+                    cnt -= 1
                 else:
                     ret += 1
             
             i += 1
             
-        return ret + len(stk) * 2
+        return ret + cnt * 2
     
