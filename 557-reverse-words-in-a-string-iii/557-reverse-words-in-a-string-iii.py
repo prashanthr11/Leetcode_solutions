d7e08a1030281b0fd7b758f95aebbfcd499ee25f
Prashanth Reddy <39459337+prashanthr11@users.noreply.github.com>
2022-09-21 22:17:24 -0700

    Time: 95 ms (31.18%), Space: 14.4 MB (97.19%) - LeetHub

--- 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.py
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.py
@@ -1,3 +1,31 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
-        return " ".join([i[::-1] for i in s.split()])
+        return self.optimise(s)
+    
+    def optimise(self, s):
+        '''
+        Time Complexity: O(N)
+        Space Complexity: O(N)
+        '''
+        ln = len(s)
+        ret = [" "] * ln
+        
+        i, pos = 0, 0
+        
+        while i < ln:
+            j = i
+            while j < ln and s[j] != " ":
+                j += 1
+                
+            tmp = j - 1
+            while tmp >= i:
+                ret[pos] = s[tmp]
+                pos += 1
+                tmp -= 1
+                
+            i = j + 1
+            pos += 1
+            
+        return "".join(ret)
+    
+            
