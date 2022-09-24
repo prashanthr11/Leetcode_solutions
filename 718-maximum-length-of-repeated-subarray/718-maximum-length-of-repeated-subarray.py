f6fe775b8fb4a74d4eb9f727afe4ea6b990641d5
Prashanth Reddy <39459337+prashanthr11@users.noreply.github.com>
2022-09-23 19:50:02 -0700

    Time: 7791 ms (22.71%), Space: 13.9 MB (89.76%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.py
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.py
@@ -1,26 +1,47 @@
 class Solution:
     def findLength(self, nums1: List[int], nums2: List[int]) -> int:
-        return self.solve(nums1, nums2)
-    
-    def solve(self, nums1, nums2):
+        return self.optimise(nums1, nums2)
+        
+    def dp(self, nums1, nums2):
         '''
         Time Complexity: O(M*N)
         Space Complexity: O(M*N)
         '''
         n, m = len(nums1), len(nums2)
-        dp = [[0]*(m + 1) for i in range(n + 1)]
         
-        for i in range(n - 1, -1, -1):
-            for j in range(m - 1, -1, -1):
+        dp = [[0]*(n + 1) for i in range(m + 1)]
+        
+        for i in range(n):
+            for j in range(m):
                 if nums1[i] == nums2[j]:
-                    dp[i][j] = 1 + dp[i + 1][j + 1]
-
+                    dp[i + 1][j + 1] = 1 + dp[i][j]
+                    
+        return max([max(i) for i in dp])
+    
+    def optimise(self, nums1, nums2):
+        '''
+        Time Complexity: O(M*N)
+        Space Complexity: O(1)
+        '''
+        B, A = sorted([nums2, nums1], key=len)
+        n, m = len(B), len(A)
         maxi = 0
-        for xx in dp:
-            maxi = max(maxi, max(xx))
-            
+        
+        for i in range(-n + 1, n + m - 1):
+            cnt = 0
+            for j in range(n):
+                top_idx = i + j
+                if top_idx < 0:
+                    continue
+                    
+                if top_idx >= m:
+                    break
+                    
+                if B[j] == A[top_idx]:
+                    cnt += 1
+                    maxi = max(maxi, cnt)
+                else:
+                    cnt = 0
+                    
         return maxi
     
-
-        
-        
