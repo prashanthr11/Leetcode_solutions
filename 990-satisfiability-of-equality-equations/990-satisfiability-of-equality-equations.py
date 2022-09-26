24f50cbf07f3846b5f45cc7752d91abbc3d17f31
Prashanth Reddy <39459337+prashanthr11@users.noreply.github.com>
2022-09-25 18:49:05 -0700

    Time: 111 ms (8.35%), Space: 14.1 MB (69.95%) - LeetHub

---
+++ 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.py
@@ -0,0 +1,40 @@
+class UnionFind:
+    def __init__(self, n):
+        self.parent = [i for i in range(n)]
+        
+    def find(self, n):
+        while self.parent[n] != n:
+            n = self.parent[n]
+            
+        return self.parent[n]
+    
+    def union(self, a, b):
+        x, y = self.find(a), self.find(b)
+        self.parent[y] = x
+        
+        return True
+
+class Solution:
+    def equationsPossible(self, equations: List[str]) -> bool:
+        union_find = UnionFind(26)
+        flag = True
+        
+        for a, b, c, d in equations:
+            a_ordd, d_ordd = ord(a) - ord('a'), ord(d) - ord('a')
+            
+            if b == "=":
+                res = union_find.union(a_ordd, d_ordd)
+                if not res:
+                    flag = False
+        
+        for a, b, c, d in equations:
+            if b == "!":
+                a_ordd, d_ordd = ord(a) - ord('a'), ord(d) - ord('a')
+                
+                x, y = union_find.find(a_ordd), union_find.find(d_ordd)
+                
+                if x == y:
+                    flag = False
+
+        return flag
+    
