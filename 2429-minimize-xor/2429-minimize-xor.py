3f38219126760e2fa6a897878893fda231c1b2a3
Prashanth Reddy <39459337+prashanthr11@users.noreply.github.com>
2022-10-02 14:53:45 -0700

    Time: 52 ms (40.00%), Space: 14.1 MB (10.00%) - LeetHub

--- 2429-minimize-xor/2429-minimize-xor.py
+++ 2429-minimize-xor/2429-minimize-xor.py
@@ -1,6 +1,32 @@
 class Solution:
     def minimizeXor(self, num1: int, num2: int) -> int:
-        # return self.naive(num1, num2)
+        return self.optimise(num1, num2)
+        
+        
+    def optimise(self, num1, num2):
+        a, b = num1.bit_count(), num2.bit_count()
+        
+        if a == b:
+            return num1
+        
+        ret = 0
+        for i in range(31, -1, -1):
+            if b > 0 and num1 & (1 << i):
+                ret |= (1 << i)
+                b -= 1
+                
+        for i in range(32):
+            if b == 0:
+                break
+                
+            if num1 & (1 << i) == 0:
+                ret |= (1 << i)
+                b -= 1
+                
+        return ret
+        
+    
+    def naive(self, num1, nums2):
         one_bin = bin(num1)[2:]
         two_bin = bin(num2)[2:]
         
@@ -49,59 +75,3 @@ class Solution:
                 
         return ret
             
-        
-    def naive(self, num1, num2):
-        bits = self.get_set_bits(num2)
-        ones_bits = self.get_set_bits(num1)
-
-        if ones_bits < bits:
-            ret = 0
-            binn = bin(num1)[2:][::-1]
-            zeros = [i for i in range(len(binn)) if binn[i] != "1"]
-            ones = [i for i in range(len(binn)) if binn[i] == "1"][::-1]
-            ones_pos = len(ones) - 1
-            zeros_pos = 0
-            
-            while bits:
-                if ones_pos < 0:
-                    if zeros_pos < len(zeros):
-                        ret |= (1 << zeros[zeros_pos])
-                        zeros_pos += 1
-                    else:
-                        ret <<= 1
-                        ret |= 1
-                else:
-                    if ones_pos == len(ones) - 1:
-                        ret = 1 << ones[ones_pos]
-                    else:
-                        tmp = 1 << ones[ones_pos]
-                        ret |= tmp
-                    ones_pos -= 1
-                bits -= 1
-                
-        elif ones_bits == bits:
-            ret = num1
-        else:
-            ret = 0
-            binn = bin(num1)[2:][::-1]
-            lst = [i for i in range(len(binn)) if binn[i] == "1"]
-            pos = len(lst) - 1
-            while bits:
-                if pos == len(lst) - 1:
-                    ret = 1 << lst[pos]
-                else:
-                    tmp = 1 << lst[pos]
-                    ret |= tmp
-                bits -= 1
-                pos -= 1
-            
-        return ret
-    
-    def get_set_bits(self, x):
-        cnt = 0
-        
-        while x:
-            cnt = cnt + 1 if x & 1 else cnt
-            x >>= 1
-            
-        return cnt
