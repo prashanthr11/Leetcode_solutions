3ccda692ea248635bbc819c6ca1fecacc61bbe1a
Prashanth Reddy <39459337+prashanthr11@users.noreply.github.com>
2022-10-10 22:31:30 -0700

    Time: 838 ms (69.12%), Space: 27.1 MB (5.42%) - LeetHub

---
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.py
@@ -0,0 +1,44 @@
+class Solution:
+    def increasingTriplet(self, nums: List[int]) -> bool:
+        return self.test(nums)
+    
+    
+    def naive(self, nums):
+        '''
+        Time Complexity: O(N^3)
+        Space Complexity: O(1)
+        '''
+        ln = len(nums)
+        
+        for i in range(ln):
+            for j in range(i + 1, ln):
+                if nums[i] < nums[j]:
+                    for k in range(j + 1, ln):
+                        if nums[j] < nums[k]:
+                            return True
+
+        return False
+    
+    def test(self, nums, idx=0):
+        '''
+        Time Complexity: O(N)
+        Space Complexity: O(N)
+        '''
+        ln = len(nums)
+        minis = [0] * ln
+        maxis = [0] * ln
+        
+        for i in range(ln):
+            if not i:
+                minis[i] = nums[i]
+                maxis[ln - i - 1] = nums[ln - i - 1]
+            else:
+                minis[i] = min(nums[i], minis[i - 1])
+                maxis[ln - i - 1] = max(nums[ln - i - 1], maxis[ln - i])
+                        
+        for i in range(ln):
+            if minis[i] < nums[i] < maxis[i]:
+                return True
+            
+        return False
+    
