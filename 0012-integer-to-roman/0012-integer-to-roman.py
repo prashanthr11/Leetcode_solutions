3287cee8694273a9976fe92e30e1a8d510667bb8
Prashanth Reddy <39459337+prashanthr11@users.noreply.github.com>
2022-10-20 10:10:15 -0700

    Time: 86 ms (62.96%), Space: 14 MB (6.54%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.py
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -2,6 +2,26 @@ from bisect import bisect
 
 class Solution:
     def intToRoman(self, num: int) -> str:
+        return self.method_2(num)
+
+    def method_2(self, num):
+        '''
+        Time Complexity: O(1)
+        Space Complexity: O(1)
+        '''
+        M = ["", "M", "MM", "MMM"]
+        C = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
+        X = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
+        I = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
+        
+        return M[num // 1000] + C[(num % 1000) // 100] + X[(num % 100) // 10] + I[num % 10]
+    
+        
+    def method_1(self, num):
+        '''
+        Time Complexity: O(Log Log N) -> Log N for binary search and Log N for creating roman numeral for each digit
+        Space Complexity: Not sure! coz it is dependent on each digit. Eg: 7 vs 8 vs 9
+        '''
         mapping = {
             1: "I",
             5: "V",
@@ -12,43 +32,43 @@ class Solution:
             1000: "M",
         }
         
-        keys = list(mapping.keys())
-        ret = ""
+        keys = [1, 5, 10, 50, 100, 500, 1000]
+        ret = []
         
         while num:
             if num == 4:
-                ret += "IV"
+                ret.append("IV")
                 break
 
             if num == 9:
-                ret += "IX"
+                ret.append("IX")
                 break
             
             if 40 <= num <= 49:
-                ret += "XL"
+                ret.append("XL")
                 num %= 10
                 continue
             
             if 90 <= num <= 99:
-                ret += "XC"
+                ret.append("XC")
                 num %= 10
                 continue
                 
             if 400 <= num <= 499:
-                ret += "CD"
+                ret.append("CD")
                 num %= 100
                 continue
             
             if 900 <= num <= 999:
-                ret += "CM"
+                ret.append("CM")
                 num %= 100
                 continue
                 
             idx = bisect(keys, num)
             max_currency = keys[idx - 1]
             symbol = mapping[max_currency]
-            ret += symbol
+            ret.append(symbol)
             num -= max_currency
             
-        return ret
+        return "".join(ret)
     
