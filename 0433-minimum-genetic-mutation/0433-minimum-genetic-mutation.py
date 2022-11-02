a81e86c46dec6ba3bce5cc378418a6cd2fcfc87d
Prashanth Reddy <39459337+prashanthr11@users.noreply.github.com>
2022-11-01 19:17:26 -0700

    Time: 60 ms (34.76%), Space: 14.1 MB (5.18%) - LeetHub

--- 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.py
+++ 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.py
@@ -1,40 +1,69 @@
-class Solution:
-    def minMutation(self, start: str, end: str, bank: List[str]) -> int:
-        return self.naive(start, end, bank)
+class state:
+    def __init__(self, g_of_n, word, end):
+        self.g_of_n = g_of_n
+        self.word = word
+        self.h_of_n = self.calculate_heuristic(word, end)
+        self.cost = self.h_of_n + g_of_n
+        
+    def calculate_heuristic(self, word, end):
+        i, j, n, m = 0, 0, len(word), len(end)
+        cnt = 0
+        
+        while i < n and j < m:
+            cnt += word[i] != end[j]
+            i += 1
+            j += 1
+            
+        return cnt
     
-    def naive(self, start, end, bank):
-        def is_successor(a, b):
-            i, j, n, m = 0, 0, len(a), len(b)
-            cnt = 0
+    def __lt__(self, other):
+        return self.cost < other.cost
+
+class AStar:
+    def is_successor(self, word1, word2):
+        i, j, n, m = 0, 0, len(word1), len(word2)
+        cnt = 0
+
+        while i < n and j < m:
+            if word1[i] != word2[j]:
+                cnt += 1
+
+            i += 1
+            j += 1
+
+        return cnt + (i - n) + (j - m) == 1
+
+    def successors(self, strt, bank):
+        ret = []
+        for word in bank:
+            if self.is_successor(word, strt):
+                ret.append(word)
+
+        return ret
+    
+    def solve(self, start, end, bank):
+        dq = [state(0, start, end)]
+        
+        visited = set()
+        while len(dq):
+            state_obj = heappop(dq)
             
-            while i < n and j < m:
-                if a[i] != b[j]:
-                    cnt += 1
-                    
-                i += 1
-                j += 1
+            if state_obj.word == end:
+                return state_obj.cost
+            
+            if state_obj.word in visited:
+                continue
                 
-            return cnt + (i - n) + (j - m) == 1
-        
-        def successors(strt):
-            ret = []
-            for word in bank:
-                if is_successor(word, strt):
-                    ret.append(word)
-                    
-            return ret
-        
-        dq = deque([(start, [], 0)])
-        
-        while dq:
-            top, visited, cnt = dq.popleft()
+            visited.add(state_obj.word)
             
-            if top == end:
-                return cnt
-            
-            for i in successors(top):
+            for i in self.successors(state_obj.word, bank):
                 if i not in visited:
-                    dq.append((i, visited + [top], cnt + 1))
+                    heappush(dq, state(state_obj.g_of_n + 1, i, end))
                     
         return -1
-    
+
+
+class Solution:
+    def minMutation(self, start: str, end: str, bank: List[str]) -> int:
+        return AStar().solve(start, end, bank)
+     
