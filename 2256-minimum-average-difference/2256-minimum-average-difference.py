6a5301492b1be69124dace9b28e64cf357b00576
Prashanth Reddy <39459337+prashanthr11@users.noreply.github.com>
2022-12-03 20:45:59 -0800

    Time: 1373 ms (78.56%), Space: 24.9 MB (80.12%) - LeetHub

--- 2256-minimum-average-difference/2256-minimum-average-difference.py
+++ 2256-minimum-average-difference/2256-minimum-average-difference.py
@@ -2,7 +2,7 @@ class Solution:
     def minimumAverageDifference(self, nums: List[int]) -> int:
         '''
         Time Complexity: O(N)
-        Space Complexity: O(N)
+        Space Complexity: O(1)
         '''
         n = len(nums)
         mini = float('inf')
@@ -15,7 +15,7 @@ class Solution:
             last = total_sum - sumi
             
             first_avg = int(sumi / (i + 1))
-            last_avg = int(last / (n - i - 1)) if last else 0
+            last_avg = int(last / (n - i - 1)) if i + 1 != n else 0
             
             ans = abs(first_avg - last_avg)
             if ans < mini:
