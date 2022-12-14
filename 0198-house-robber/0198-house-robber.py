7c86444bc16846ad04f557652b7bf0c862dd9baa
Prashanth Reddy <39459337+prashanthr11@users.noreply.github.com>
2022-12-13 20:54:05 -0800

    Time: 33 ms (92.06%), Space: 13.8 MB (66.57%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,16 +1,20 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
+        '''
+        Time Complexity: O(N)
+        Space Complexity: O(1)
+        '''
         n = len(nums)
         
         if n <= 2:
             return max(nums)
         
-        dp = [float('inf')] * n
-        dp[0] = nums[0]
-        dp[1] = max(nums[0], nums[1])
+        day_before_yesterday, yesterday = nums[0], max(nums[0], nums[1])
         
         for i in range(2, n):
-            dp[i] = max(nums[i] + dp[i - 2], dp[i - 1])
+            temp = yesterday
+            yesterday = max(day_before_yesterday + nums[i], yesterday)
+            day_before_yesterday = temp
             
-        return dp[-1]
+        return yesterday
     
