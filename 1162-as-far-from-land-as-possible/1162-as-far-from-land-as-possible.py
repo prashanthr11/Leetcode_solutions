e1172ed5f400a218448073d1c9a8fa740db02009
Prashanth Reddy <39459337+prashanthr11@users.noreply.github.com>
2023-02-09 22:15:43 -0800

    Time: 651 ms (58.99%), Space: 14.9 MB (47.72%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.py
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.py
@@ -3,20 +3,18 @@ class Solution:
         n = len(grid)
         dq = deque([(i, j, 0) for i in range(n) for j in range(n) if grid[i][j]])
         dist = [[0] * n for i in range(n)]
-        visited = [[0] * n for i in range(n)]
         neighbours = [(-1, 0), (1, 0), (0, -1), (0, 1)]
         
         while dq:
             i, j, cost = dq.popleft()
             
-            if visited[i][j]:
+            if dist[i][j]:
                 continue
                 
             dist[i][j] = cost
-            visited[i][j] = True
             
             for x, y in neighbours:
-                if 0 <= i + x < n and 0 <= j + y < n and not visited[i + x][j + y]:
+                if 0 <= i + x < n and 0 <= j + y < n and grid[i + x][j + y] == 0 and dist[i + x][j + y] == 0:
                     dq.append((i + x, j + y, cost + 1))
         
         maxi = -1
