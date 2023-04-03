a0ad23aef0c09436ceb8572d16b8a0102458cc98
Prashanth Reddy <39459337+prashanthr11@users.noreply.github.com>
2023-04-02 17:51:10 -0700

    Time: 1804 ms (60.84%), Space: 37 MB (83.38%) - LeetHub

--- 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.py
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.py
@@ -10,7 +10,19 @@ class Solution:
         ret = [0] * ln
         
         for i in range(ln):
-            ret[i] = ln_potions - bisect.bisect_left(potions, math.ceil(success/spells[i]))
+            ret[i] = ln_potions - self.find_ele(potions, math.ceil(success/spells[i]))
             
         return ret
     
+    def find_ele(self, lst, val):
+        low, high = 0, len(lst)
+        
+        while low < high:
+            mid = (low + high) // 2
+            
+            if lst[mid] < val:
+                low = mid + 1
+            else:
+                high = mid
+                
+        return low
