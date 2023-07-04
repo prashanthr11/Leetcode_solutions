9eab81a1585d34d54916e6ffe2b200af6e511656
Prashanth Reddy <39459337+prashanthr11@users.noreply.github.com>
2023-07-03 23:05:28 -0700

    Time: 76 ms (61.14%), Space: 18.1 MB (86.89%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.py
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -1,5 +1,18 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
+        return self.optimise(nums)
+    
+    def optimise(self, nums):
+        ln = len(nums)
+        nums.sort()
+        
+        for i in range(0, ln, 3):
+            if i + 1 < ln and nums[i] != nums[i + 1]:
+                return nums[i]
+        
+        return nums[i]
+    
+    def naive(self, nums):
         d = Counter(nums)
         
         for k, v in d.items():
