3ed268522432653c8f1ef1a9d9d770b847d67e10
Prashanth Reddy <39459337+prashanthr11@users.noreply.github.com>
2023-07-25 23:06:11 -0700

    Time: 5582 ms (10.54%), Space: 30.9 MB (25.64%) - LeetHub

---
+++ 1870-minimum-speed-to-arrive-on-time/1870-minimum-speed-to-arrive-on-time.py
@@ -0,0 +1,33 @@
+class Solution:
+    def minSpeedOnTime(self, dist: List[int], hour: float) -> int:
+        low, high = 1, 10**9
+        ln = len(dist)
+        
+        def solve(n):
+            cnt = 0
+            
+            for i, a in enumerate(dist):
+                
+                if cnt > hour:
+                    break
+                
+                if i + 1 != ln:
+                    cnt += ceil(a / n)
+                else:
+                    cnt += (a / n)
+                    
+            return cnt > hour
+        
+        while low < high:
+            # print(low, high)
+            mid = low + (high - low) // 2
+            
+            if solve(mid) == False:
+                high = mid
+            else:
+                low = mid + 1
+                
+        res = solve(low)
+        # print(low, res)
+        return -1 if res else low
+            
