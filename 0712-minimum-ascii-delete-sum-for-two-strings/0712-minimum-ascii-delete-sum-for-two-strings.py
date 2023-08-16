b3609a969cebc4dfa4c234a9e27f828d43a2a1a0
Prashanth Reddy <39459337+prashanthr11@users.noreply.github.com>
2023-08-16 12:39:45 -0700

    Time: 818 ms (40.85%), Space: 221.4 MB (25.00%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.py
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.py
@@ -1,31 +1,26 @@
 class Solution:
     def minimumDeleteSum(self, s1: str, s2: str) -> int:
         l1, l2 = len(s1), len(s2)
-        dp = {}
         
+        @cache
         def solve(x, y):
+            sumi = 0
             
-            if x == l1 and y == l2:
+            if x >= l1 and y >= l2:
                 return 0
             
             if x == l1:
-                dp[(x, y)] = ord(s2[y]) + solve(x, y + 1)
-                return dp[(x, y)]
+                return ord(s2[y]) + solve(x, y + 1)
             
             if y == l2:
-                dp[(x, y)] = ord(s1[x]) + solve(x + 1, y)
-                return dp[(x, y)]
-                
-            if (x, y) in dp:
-                return dp[(x, y)]
+                return ord(s1[x]) + solve(x + 1, y)
+            
             
             if s1[x] == s2[y]:
-                dp[(x, y)] = solve(x + 1, y + 1)
+                return solve(x + 1, y + 1)
             else:
-                dp[(x, y)] = min(ord(s1[x]) + solve(x + 1, y),
-                      ord(s2[y]) + solve(x, y + 1))
-            
-            return dp[(x, y)]
+                return min(ord(s1[x]) + solve(x + 1, y),
+                           ord(s2[y]) + solve(x, y + 1))
         
         return solve(0, 0)
     
