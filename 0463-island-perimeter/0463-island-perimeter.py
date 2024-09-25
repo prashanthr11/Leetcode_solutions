1622df46504d099cd5fbe3c6a5980b618f16d1fa
Prashanth Reddy <39459337+prashanthr11@users.noreply.github.com>
2024-09-25 15:21:31 -0700

    Time: 519 ms (7.77%), Space: 18.9 MB (21.95%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.py
+++ 0463-island-perimeter/0463-island-perimeter.py
@@ -1,20 +1,41 @@
 class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
-        cnt = 0
-        rows, cols = len(grid), len(grid[0])
+        return self.optimise(grid)
+    
+    
+    def optimise(self, grid):
+        r, c = -1, -1
         
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j]:
+                    r, c = i, j
+                    break
+                    
         def get_count(r, c):
-            ret = 4
+            lst = []
             for nr, nc in [(r + 1, c), (r - 1, c), (r, c + 1), (r, c - 1)]:
-                if 0 <= nr < rows and 0 <= nc < cols and grid[nr][nc] == 1:
-                    ret -= 1
+                if 0 <= nr < len(grid) and 0 <= nc < len(grid[0]) and grid[nr][nc] == 1:
+                    lst.append((nr, nc))
                     
-            return ret
+            return lst
         
-        for row in range(rows):
-            for col in range(cols):
-                if grid[row][col]:
-                    cnt += get_count(row, col)
-                    
-        return cnt
+        ret = 0
+        
+        queue = [(r, c)]
+        visited = set()
+        
+        while queue:
+            nr, nc = queue.pop()
+            
+            if (nr, nc) in visited:
+                continue
+                
+            visited.add((nr, nc))
+            
+            lst = get_count(nr, nc)
+            queue.extend(lst)
+            ret += 4 - len(lst)
+            
+        return ret
     
