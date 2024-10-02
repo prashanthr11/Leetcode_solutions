368015490b597e4b0f9f6664cb9dc90b7d216d23
Prashanth Reddy <39459337+prashanthr11@users.noreply.github.com>
2024-10-02 11:35:36 -0700

    Time: 892 ms (19.59%), Space: 30.4 MB (72.49%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.py
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.py
@@ -2,20 +2,20 @@ class Solution:
     def minOperations(self, nums: List[int], x: int) -> int:
 
         new_target = sum(nums) - x
-        window = deque([])
+        left = 0
         ans = float('inf')
         window_sum = 0
         
         for i, a in enumerate(nums):
             
             window_sum += a
-            window.append(i)
             
-            while window and window_sum > new_target:
-                window_sum -= nums[window.popleft()]
+            while left <= i and window_sum > new_target:
+                window_sum -= nums[left]
+                left += 1
                 
             if window_sum == new_target:
-                no_of_ops = len(nums) - len(window)
+                no_of_ops = len(nums) - (i - left + 1)
                 ans = min(ans, no_of_ops)
                 
         return ans if ans != float('inf') else -1
